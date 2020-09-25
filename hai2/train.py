64318c2b36dd2169d0d5d8c3a7d2fe3eb38f558b
Youngrok Song <id2thomas@gmail.com>
2020-09-25 15:10:23 +0900

    start with train data loader

--- hai2/train.py
+++ hai2/train.py
@@ -1,4 +1,3 @@
-#HAI1 RNN Train file
 import sys
 
 from pathlib import Path
@@ -6,11 +5,21 @@ from datetime import timedelta
 
 import dateutil
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import torch
 
+import matplotlib.pyplot as plt
+
+#My Utils
+
+import random
+
+import torch
+from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
+
+#Performance Metrics
+from TaPR_pkg import etapr
+
 import argparse
 
 parser = argparse.ArgumentParser(description='HAI Train Params')
@@ -22,31 +31,38 @@ parser.add_argument('--hid', type=int, default=100,
                     help='RNN Hidden Size')
 parser.add_argument('--n_l', type=int, default=3,
                     help='Number of RNN Layers')
-parser.add_argument('--win', type=int, default=90,
+parser.add_argument('--win', type=int, default=60,
                     help='Window Size')
-parser.add_argument('--ldim', type=int, default=8,
-                    help='Latent dim')
-parser.add_argument('--m_name', type=str, default='baseline',
-                    help='Model Name')
+parser.add_argument('--st', type=int, default=3,
+                    help='Stride')
+parser.add_argument('--th', type=float, default='0.04',
+                    help='Threshold')
+parser.add_argument('--seed', type=int, default=42,
+                    help='Random Seed')
 args = parser.parse_args()
 
-#Set Params
+DEVICE = torch.device('cuda:0')
+SEED=args.seed
+
+#Set Random Seed
+torch.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+random.seed(SEED)
+
+#Import Models
+from models import *
+
+#Get Arguments
 WINDOW_SIZE = args.win
 WINDOW_GIVEN = WINDOW_SIZE-1
+STRIDE=args.st
 
-N_HIDDENS = args.hid
-N_LAYERS = args.n_l
-L_DIM=args.ldim
 
-MAX_EPOCHS = args.ep
-BATCH_SIZE = args.batch
-
-MODEL_NAME=args.m_name
-
-#Data Preprocess
+#Load Train Data
 TRAIN_DATASET = sorted([x for x in Path('./data/training/').glob("*.csv")])
-VAL_DATASET = sorted([x for x in Path('./data/validation/').glob("*.csv")])
-TEST_DATASET = sorted([x for x in Path('./data/testing/').glob("*.csv")])
 
 def dataframe_from_csv(target):
     return pd.read_csv(target).rename(columns=lambda x: x.strip())
@@ -55,13 +71,9 @@ def dataframe_from_csvs(targets):
     return pd.concat([dataframe_from_csv(x) for x in targets])
 
 TRAIN_DF_RAW = dataframe_from_csvs(TRAIN_DATASET)
-VAL_DF_RAW = dataframe_from_csvs(VAL_DATASET)
-TEST_DF_RAW = dataframe_from_csvs(TEST_DATASET)
 
 TIMESTAMP_FIELD = "time"
 ATTACK_FIELD = "attack"
-
-VALID_COLUMNS_IN_TRAIN_DATASET = TRAIN_DF_RAW.columns.drop([TIMESTAMP_FIELD])
 
 #Min Max Vals from Train data -> Normalize Train,Test Dataset
 TAG_MIN = TRAIN_DF_RAW[VALID_COLUMNS_IN_TRAIN_DATASET].min()
@@ -81,114 +93,3 @@ def boundary_check(df):
     return np.any(x > 1.0), np.any(x < 0), np.any(np.isnan(x))
 
 TRAIN_DF = normalize(TRAIN_DF_RAW[VALID_COLUMNS_IN_TRAIN_DATASET]).ewm(alpha=0.9).mean()
-
-#Make Torch Data
-
-class HaiDataset(Dataset):
-    def __init__(self, timestamps, df, stride=1, attacks=None):
-        self.ts = np.array(timestamps)
-        self.tag_values = np.array(df, dtype=np.float32)
-        self.valid_idxs = []
-        for L in range(len(self.ts) - WINDOW_SIZE + 1):
-            R = L + WINDOW_SIZE - 1
-            if dateutil.parser.parse(self.ts[R]) - dateutil.parser.parse(
-                self.ts[L]
-            ) == timedelta(seconds=WINDOW_SIZE - 1):
-                self.valid_idxs.append(L)
-        self.valid_idxs = np.array(self.valid_idxs, dtype=np.int32)[::stride]
-        self.n_idxs = len(self.valid_idxs)
-        print(f"# of valid windows: {self.n_idxs}")
-        if attacks is not None:
-            self.attacks = np.array(attacks, dtype=np.float32)
-            self.with_attack = True
-        else:
-            self.with_attack = False
-
-    def __len__(self):
-        return self.n_idxs
-
-    def __getitem__(self, idx):
-        i = self.valid_idxs[idx]
-        last = i + WINDOW_SIZE - 1
-        item = {"attack": self.attacks[last]} if self.with_attack else {}
-        item["ts"] = self.ts[i + WINDOW_SIZE - 1]
-        item["given"] = torch.from_numpy(self.tag_values[i : i + WINDOW_GIVEN])
-        item["answer"] = torch.from_numpy(self.tag_values[last])
-        return item
-
-
-HAI_DATASET_TRAIN = HaiDataset(TRAIN_DF_RAW[TIMESTAMP_FIELD], TRAIN_DF, stride=10)
-
-#Model
-if MODEL_NAME=='baseline':
-    from baseline_model import HAIModel
-    loss_fn = torch.nn.MSELoss()
-
-elif MODEL_NAME=='vae':
-    from vae_model import HAIModel
-
-model_params={
-    'n_tags':TRAIN_DF.shape[1],
-    'n_hid': N_HIDDENS,
-    'n_layers':N_LAYERS,
-    'l_dim':L_DIM
-}
-
-MODEL = HAIModel(model_params)
-MODEL.cuda()
-
-def train(dataset, model, batch_size, n_epochs):
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    optimizer = torch.optim.AdamW(model.parameters())
-
-    epochs = range(n_epochs)
-    best = {"loss": sys.float_info.max}
-    loss_history = []
-    for e in epochs:
-        epoch_loss = 0
-        for batch in dataloader:
-            optimizer.zero_grad()
-            given = batch["given"].cuda()
-            if MODEL_NAME=='baseline':
-                guess = MODEL(given)
-                answer = batch["answer"].cuda()
-                loss = loss_fn(answer, guess)
-            elif MODEL_NAME=='vae':
-                guess,mu,var = MODEL(given)
-                answer = batch["answer"].cuda()
-                loss = MODEL.loss_fn(mu,var,answer, guess)
-
-            loss.backward()
-            epoch_loss += loss.item()
-            optimizer.step()
-        print('Epoch {} Loss {:.6f}'.format(e+1,epoch_loss))
-        loss_history.append(epoch_loss)
-
-        #For validation Loss
-        #with torch.no_grad():
-        # epochs.set_postfix_str(f"loss: {epoch_loss:.6f}")
-        if epoch_loss < best["loss"]:
-            best["state"] = model.state_dict()
-            best["loss"] = epoch_loss
-            best["epoch"] = e + 1
-    return best, loss_history
-
-
-######## Train #######
-MODEL.train()
-
-BEST_MODEL, LOSS_HISTORY = train(HAI_DATASET_TRAIN, MODEL, BATCH_SIZE, MAX_EPOCHS)
-
-print(BEST_MODEL["loss"], BEST_MODEL["epoch"])
-
-with open("./model/{}.pt".format(MODEL_NAME), "wb") as f:
-    torch.save(
-        {
-            "state": BEST_MODEL["state"],
-            "best_epoch": BEST_MODEL["epoch"],
-            "loss_history": LOSS_HISTORY,
-        },
-        f,
-    )
-
-######## Train #######
---
+++ hai2/utils/data_utils.py
@@ -0,0 +1,45 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+
+def load_processed(type,only_data=False):
+    data=np.load('./processed/'+type+'.npy')
+    if only_data:
+        return data
+    else:
+        label=np.load('./processed/'+type+'_atk.npy')
+        return data,label
+
+class HaiDataset(Dataset):
+    def __init__(self, timestamps, df, stride=1, attacks=None):
+        self.ts = np.array(timestamps)
+        self.tag_values = np.array(df, dtype=np.float32)
+        self.valid_idxs = []
+        for L in range(len(self.ts) - WINDOW_SIZE + 1):
+            R = L + WINDOW_SIZE - 1
+            if dateutil.parser.parse(self.ts[R]) - dateutil.parser.parse(
+                self.ts[L]
+            ) == timedelta(seconds=WINDOW_SIZE - 1):
+                self.valid_idxs.append(L)
+        self.valid_idxs = np.array(self.valid_idxs, dtype=np.int32)[::stride]
+        self.n_idxs = len(self.valid_idxs)
+        print(f"# of valid windows: {self.n_idxs}")
+        if attacks is not None:
+            self.attacks = np.array(attacks, dtype=np.float32)
+            self.with_attack = True
+        else:
+            self.with_attack = False
+
+    def __len__(self):
+        return self.n_idxs
+
+    def __getitem__(self, idx):
+        i = self.valid_idxs[idx]
+        last = i + WINDOW_SIZE - 1
+        item = {"attack": self.attacks[last]} if self.with_attack else {}
+        item["ts"] = self.ts[i + WINDOW_SIZE - 1]
+        item["given"] = torch.from_numpy(self.tag_values[i : i + WINDOW_GIVEN])
+        item["answer"] = torch.from_numpy(self.tag_values[last])
+        return item
